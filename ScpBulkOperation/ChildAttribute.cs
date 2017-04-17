9a1708dfba2e3b2de6e555c58779bb539b6ed790
WIV2 <WIV2@IN01LAP18604>
2017-04-17 14:48:29 +0530

    Add project files.

---
+++ ScpBulkOperation/ChildAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkOperation
+{
+    public class ChildAttribute : Attribute
+    {
+        public string Name { get; set; }
+    }
+}
